c723dcd5a5b8b497449f8cb856ee7619d93d4b1b
jbaldwin <joshrbaldwin@gmail.com>
2012-11-23 15:37:14 -0700

    Updated b2Color with comments

--- common/b2Color.d.ts
+++ common/b2Color.d.ts
@@ -1,13 +1,45 @@
 module Box2D.Common {
+	
+	/**
+	* Color for debug drawing.  Each value has the range [0, 1].
+	**/
 	export class b2Color {
 		
+		/**
+		* Red
+		**/
 		public r: number;
+
+		/**
+		* Green
+		**/
 		public g: number;
+
+		/**
+		* Blue
+		**/
 		public b: number;
+
+		/**
+		* RGB color as hex.
+		* @type uint
+		**/
 		public color: number;
 
-		constructor (r: number, g: number, b: number);
+		/**
+		* Constructor
+		* @rr Red value
+		* @gg Green value
+		* @bb Blue value
+		**/
+		constructor (rr: number, gg: number, bb: number);
 
-		public Set(r: number, g: number, b: number): void;
+		/**
+		* Sets the Color to new RGB values.
+		* @rr Red value
+		* @gg Green value
+		* @bb Blue value
+		**/
+		public Set(rr: number, gg: number, bb: number): void;
 	}
 }
