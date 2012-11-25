793b57d45886c5fb2bf135b2903cd850407d87c9
jbaldwin <joshrbaldwin@gmail.com>
2012-11-24 22:10:22 -0700

    Updated b2CircleShape definitions
    Also added comments

--- Collision/Shapes/b2CircleShape.d.ts
+++ Collision/Shapes/b2CircleShape.d.ts
@@ -1,7 +1,103 @@
+/// <reference path="../../Common/Math/b2Transform.d.ts" />
+/// <reference path="../../Common/Math/b2Vec2.d.ts" />
+/// <reference path="../b2AABB.d.ts" />
+/// <reference path="b2MassData.d.ts" />
 /// <reference path="b2Shape.d.ts" />
 
+//import b2Math = Box2D.Common.Math;
+
 module Box2D.Collision.Shapes {
+
+	/**
+	* A circle shape.
+	**/
 	export class b2CircleShape extends b2Shape {
 
+		/**
+		* Creates a new circle shape.
+		**/
+		constructor ();
+
+		/**
+		* Given a transform, compute the associated axis aligned bounding box for this shape.
+		* @aabb Calculated AABB, this argument is `out`.
+		* @xf Transform to calculate the AABB.
+		**/
+		public ComputeAABB(aabb: b2AABB, xf: b2Math.b2Transform): void;
+
+		/**
+		* Compute the mass properties of this shape using its dimensions and density. The inertia tensor is computed about the local origin, not the centroid.
+		* @massData Calculate the mass, this argument is `out`.
+		**/
+		public ComputeMass(massData: b2MassData, density: number): void;
+
+		/**
+		* Compute the volume and centroid of this shape intersected with a half plane
+		* @normal The surface normal.
+		* @offset The surface offset along the normal.
+		* @xf The shape transform.
+		* @c The centroid, this argument is `out`.
+		**/
+		public ComputeSubmergedArea(
+			normal: b2Math.b2Vec2,
+			offset: number,
+			xf: b2Math.b2Transform,
+			c: b2Math.b2Vec2): number;
+
+		/**
+		* Copies the circle shape.
+		* @return Copy of this circle shape.
+		**/
+		public Copy(): b2CircleShape;
+
+		/**
+		* Get the local position of this circle in its parent body.
+		* @return This circle's local position.
+		**/
+		public GetLocalPosition(): b2Math.b2Vec2;
+
+		/**
+		* Get the radius of the circle.
+		* @return This circle's radius.
+		**/
+		public GetRadius(): number;
+
+		/**
+		* Cast a ray against this shape.
+		* @output Ray cast results, this argument is `out`.
+		* @input Ray cast input parameters.
+		* @transform The transform to be applied to the shape.
+		* @return True if the ray hits the shape, otherwise false.
+		**/
+		public RayCast(
+			output: b2RayCastOutput,
+			input: b2RayCastInput,
+			transform: b2Math.b2Transform): bool;
+
+		/**
+		* Set the circle shape values from another shape.
+		* @other The other circle shape to copy values from.
+		**/
+		public Set(other: b2CircleShape): void;
+
+		/**
+		* Set the local position of this circle in its parent body.
+		* @position The new local position of this circle.
+		**/
+		public SetLocalPosition(position: b2Math.b2Vec2): void;
+
+		/**
+		* Set the radius of the circle.
+		* @radius The new radius of the circle.
+		**/
+		public SetRadius(radius: number): void;
+		
+		/**
+		* Test a point for containment in this shape. This only works for convex shapes.
+		* @xf Shape world transform.
+		* @p Point to test against, in world coordinates.
+		* @return True if the point is in this shape, otherwise false.
+		**/
+		public TestPoint(xf: b2Math.b2Transform, p: b2Math.b2Vec2): bool;
 	}
 }
