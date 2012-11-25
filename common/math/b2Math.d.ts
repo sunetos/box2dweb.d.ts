06f3fcf4e2e5be6dd2c3c58ec80267ea39a42226
jbaldwin <joshrbaldwin@gmail.com>
2012-11-24 18:12:00 -0700

    Updated b2Math with some comments

--- common/math/b2Math.d.ts
+++ common/math/b2Math.d.ts
@@ -3,13 +3,54 @@
 /// <reference path="b2Transform.d.ts" />
 
 module Box2D.Common.Math {
+
+	/**
+	* Math utility functions.
+	**/
 	export class b2Math {
 
+		/**
+		* Determines if a number is valid.  A number is valid if it is finite.
+		* @x Number to check for validity.
+		* @return True if x is valid, otherwise false.
+		**/
 		public static IsValid(x: number): bool;
+
+		/**
+		* Dot product of two vector 2s.
+		* @a Vector 2 to use in dot product.
+		* @b Vector 2 to use in dot product.
+		* @return Dot product of a and b.
+		**/
 		public static Dot(a: b2Vec2, b: b2Vec2): number;
+
+		/**
+		* Cross product of two vector 2s.
+		* @a Vector 2 to use in cross product.
+		* @b Vector 2 to use in cross product.
+		* @return Cross product of a and b.
+		**/
 		public static CrossVV(a: b2Vec2, b: b2Vec2): number;
+
+		/**
+		* Cross product of vector 2 and s.
+		* @a Vector 2 to use in cross product.
+		* @s s value.
+		* @return Cross product of a and s.
+		**/
 		public static CrossVF(a: b2Vec2, s: number): b2Vec2;
+
+		/**
+		* Cross product of s and vector 2.
+		* @s s value.
+		* @a Vector 2 to use in cross product.
+		* @return Cross product of s and a.
+		**/
 		public static CrossFV(s: number, a: b2Vec2): b2Vec2;
+
+		/**
+		* 
+		**/
 		public static MulMV(A: b2Mat22, v: b2Vec2): b2Vec2;
 		public static MulTMV(A: b2Mat22, v: b2Vec2): b2Vec2;
 		public static MulX(T: b2Transform, v: b2Vec2): b2Vec2;
@@ -37,8 +78,19 @@ module Box2D.Common.Math {
 		public static NextPowerOfTwo(x: number): number;
 		public static IsPowerOfTwo(x: number): bool;
 
+		/**
+		* Global instance of a zero'ed vector.  Use as read-only.
+		**/
 		public static b2Vec2_zero: b2Vec2;
+
+		/**
+		* Global instance of a 2x2 identity matrix.  Use as read-only.
+		**/
 		public static b2Mat22_identity: b2Mat22;
+
+		/**
+		* Global instance of an identity transform.  Use as read-only.
+		**/
 		public static b2Transform_identity: b2Transform;
 	}
 }
