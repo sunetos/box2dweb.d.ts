f96bde47f607708a30c9b98e2185bb8862ec3f4d
jbaldwin <joshrbaldwin@gmail.com>
2012-11-24 20:03:45 -0700

    Updated b2ManifoldPoint with comments

--- Collision/b2ManifoldPoint.d.ts
+++ Collision/b2ManifoldPoint.d.ts
@@ -4,16 +4,46 @@
 import b2Math = Box2D.Common.Math;
 
 module Box2D.Collision {
+
+	/**
+	* A manifold point is a contact point belonging to a contact manifold. It holds details related to the geometry and dynamics of the contact points. The local point usage depends on the manifold type: -e_circles: the local center of circleB -e_faceA: the local center of cirlceB or the clip point of polygonB -e_faceB: the clip point of polygonA This structure is stored across time steps, so we keep it small. Note: the impulses are used for internal caching and may not provide reliable contact forces, especially for high speed collisions.
+	**/
 	export class b2ManifoldPoint {
 
-		public m_localpoint: b2Math.b2Vec2;
+		/**
+		* Contact ID.
+		**/
 		public m_id: b2ContactID;
+
+		/**
+		* Local contact point.
+		**/
+		public m_localpoint: b2Math.b2Vec2;
+		
+		/**
+		* Normal impluse for this contact point.
+		**/
 		public m_normalImpulse: number;
+
+		/**
+		* Tangent impulse for contact point.
+		**/
 		public m_tangentImpulse: number;
 
-		public constructor ();
+		/**
+		* Creates a new manifold point.
+		**/
+		constructor ();
 
+		/**
+		* Resets this manifold point.
+		**/
 		public Reset(): void;
+
+		/**
+		* Sets this manifold point from a manifold point.
+		* @m The manifold point to copy values from.
+		**/
 		public Set(m: b2ManifoldPoint): void;
 	}
 }
