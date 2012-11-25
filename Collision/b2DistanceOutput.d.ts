065afc82c0cce0ba0a4f2ff99a0f9f758737d591
jbaldwin <joshrbaldwin@gmail.com>
2012-11-24 18:50:18 -0700

    Updated b2DistanceOutput with comments

--- Collision/b2DistanceOutput.d.ts
+++ Collision/b2DistanceOutput.d.ts
@@ -3,14 +3,30 @@
 import b2Math = Box2D.Common.Math;
 
 module Box2D.Collision {
+
+	/**
+	* Output calculation for b2Distance.
+	**/
 	export class b2DistanceOutput {
 	
-		public pointA: b2Math.b2Vec2;
-		public pointB: b2Math.b2Vec2;
+		/**
+		*  Calculated distance.
+		**/
 		public distance: number;
+
+		/**
+		* Number of gjk iterations used in calculation.
+		**/
 		public iterations: number;
 
-		constructor ();
-	
+		/**
+		* Closest point on shape A.
+		**/
+		public pointA: b2Math.b2Vec2;
+
+		/**
+		* Closest point on shape B.
+		**/
+		public pointB: b2Math.b2Vec2;
 	}
 }
