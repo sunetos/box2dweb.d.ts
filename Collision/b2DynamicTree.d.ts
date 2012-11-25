91562edf55b1f188c7ae8c821a44b2c4ed0a07a8
jbaldwin <joshrbaldwin@gmail.com>
2012-11-24 19:31:38 -0700

    Updated b2DyanmicTree with comments

--- Collision/b2DynamicTree.d.ts
+++ Collision/b2DynamicTree.d.ts
@@ -4,23 +4,75 @@
 /// <reference path="b2RayCastInput.d.ts" />
 
 module Box2D.Collision {
+	
+	/**
+	* A dynamic tree arranges data in a binary tree to accelerate queries such as volume queries and ray casts. Leafs are proxies with an AABB. In the tree we expand the proxy AABB by b2_fatAABBFactor so that the proxy AABB is bigger than the client object. This allows the client object to move by small amounts without triggering a tree update. Nodes are pooled.
+	**/
 	export class b2DynamicTree {
 
+		/**
+		* Constructing the tree initializes the node pool.
+		**/
 		constructor ();
 
+		/**
+		* Create a proxy. Provide a tight fitting AABB and a userData.
+		* @aabb AABB.
+		* @userDate User defined data for this proxy.
+		* @return Dynamic tree node.
+		**/
 		public CreateProxy(aabb: b2AABB, userData: any): b2DynamicTreeNode;
-		public DestroyProxy(proxy: b2DynamicTreeNode): void;
-		public MoveProxy(proxy: b2DynamicTreeNode, aabb: b2AABB, displacement: b2Math.b2Vec2): bool;
-		public Rebalance(iterations: number): void;
-		public GetFatAABB(proxy: b2DynamicTreeNode): b2AABB;
-		public GetUserData(proxy: b2DynamicTreeNode): any;
-		public Query(callback: (node: b2DynamicTreeNode) => bool, aabb: b2AABB): void;
-		public RayCast(callback: (subInput: b2RayCastInput, node: b2DynamicTreeNode) => number, input: b2RayCastInput): void;
 
-		// Public in box2dweb.js but they should be private functions.
-		//public AllocateNode(): b2DynamicTreeNode;
-		//public FreeNode(node: b2DynamicTreeNode): void;
-		//public InsertLeaf(leaf: b2DynamicTreeNode): void;
-		//public RemoveLeaf(leaf: b2DynamicTreeNode): void;
+		/**
+		* Destroy a proxy. This asserts if the id is invalid.
+		* @proxy Proxy to destroy.
+		**/
+		public DestroyProxy(proxy: b2DynamicTreeNode): void;
+
+		/**
+		* Gets the Fat AABB for the proxy.
+		* @proxy Proxy to retrieve Fat AABB.
+		* @return Fat AABB for proxy.
+		**/
+		public GetFatAABB(proxy: b2DynamicTreeNode): b2AABB;
+
+		/**
+		* Get user data from a proxy. Returns null if the proxy is invalid.
+		* Cast to your type on return.
+		* @proxy Proxy to retrieve user data from.
+		* @return User data for proxy or null if proxy is invalid.
+		**/
+		public GetUserData(proxy: b2DynamicTreeNode): any;
+
+		/**
+		* Move a proxy with a swept AABB. If the proxy has moved outside of its fattened AABB, then the proxy is removed from the tree and re-inserted. Otherwise the function returns immediately.
+		* @proxy Proxy to move.
+		* @aabb Swept AABB.
+		* @displacement Extra AABB displacement.
+		**/
+		public MoveProxy(proxy: b2DynamicTreeNode, aabb: b2AABB, displacement: b2Math.b2Vec2): bool;
+
+		/**
+		* Query an AABB for overlapping proxies. The callback is called for each proxy that overlaps the supplied AABB. The callback should match function signature fuction callback(proxy:b2DynamicTreeNode):Boolean and should return false to trigger premature termination.
+		* @callback Called for each proxy that overlaps the supplied AABB.
+		* @aabb Proxies are query for overlap on this AABB.
+		**/
+		public Query(callback: (node: b2DynamicTreeNode) => bool, aabb: b2AABB): void;
+
+		/**
+		* Ray-cast against the proxies in the tree. This relies on the callback to perform a exact ray-cast in the case were the proxy contains a shape. The callback also performs the any collision filtering. This has performance roughly equal to k log(n), where k is the number of collisions and n is the number of proxies in the tree.
+		* @callback Called for each proxy that is hit by the ray.
+		*	@input Ray cast input data.
+		*	@proxy The proxy hit by the ray cast.
+		*	@return Collision filtering.  (values??)
+		* @input Ray cast input data.  Query all proxies along this ray cast.
+		**/
+		public RayCast(callback: (input: b2RayCastInput, proxy: b2DynamicTreeNode) => number, input: b2RayCastInput): void;
+
+		/**
+		* Perform some iterations to re-balance the tree.
+		* @iterations Number of rebalance iterations to perform.
+		**/
+		public Rebalance(iterations: number): void;
 	}
 }
