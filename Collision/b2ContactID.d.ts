65bcb429bc2f54d0298a9e3871a3e9e8c14f75e9
jbaldwin <joshrbaldwin@gmail.com>
2012-11-24 18:40:10 -0700

    Updated b2ContactID with comments

--- Collision/b2ContactID.d.ts
+++ Collision/b2ContactID.d.ts
@@ -1,13 +1,37 @@
+/// <reference path="Features.d.ts" />
+
 module Box2D.Collision {
+
+	/**
+	* We use contact ids to facilitate warm starting.
+	**/
 	export class b2ContactID {
 
+		/**
+		* Features
+		**/
 		public features: Features;
 
-		constructor();
-
-		public Set(id: b2ContactID): void;
-		public Copy(): b2ContactID;
+		/**
+		* ID Key
+		**/
 		public Key: number;
 
+		/**
+		* Creates a new Contact ID.
+		**/
+		constructor();
+
+		/**
+		* Copies the Contact ID.
+		* @return Copied Contact ID.
+		**/
+		public Copy(): b2ContactID;
+
+		/**
+		* Sets the Contact ID from a Contact ID.
+		* @id The Contact ID to copy values from.
+		**/
+		public Set(id: b2ContactID): void;
 	}
 }
