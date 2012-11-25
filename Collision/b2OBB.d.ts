bb197ec144db4bd37dd4c5f7e0a1366e6d0f4620
jbaldwin <joshrbaldwin@gmail.com>
2012-11-24 20:07:28 -0700

    Added b2OBB definitions

---
+++ Collision/b2OBB.d.ts
@@ -0,0 +1,28 @@
+/// <reference path="../Common/Math/b2Vec2.d.ts" />
+/// <reference path="../Common/Math/b2Mat22.d.ts" />
+
+import b2Math = Box2D.Common.Math;
+
+module Box2D.Collision {
+
+	/**
+	* An oriented bounding box.
+	**/
+	export class b2OBB {
+
+		/**
+		* The local centroid.
+		**/
+		public center: b2Math.b2Vec2;
+
+		/**
+		* The half-widths.
+		**/
+		public extents: b2Math.b2Vec2;
+
+		/**
+		* The rotation matrix.
+		**/
+		public R: b2Math.b2Mat22;
+	}
+}
